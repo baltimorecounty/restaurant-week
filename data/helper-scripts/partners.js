474db8e6863e88e17d4146b382025b8ab44ca167
Marty Powell <jpowel25@gmail.com>
2017-12-15 10:26:12 -0500

    fixed data

--- data/helper-scripts/partners.js
+++ data/helper-scripts/partners.js
@@ -3,14 +3,17 @@ var partnerList = [];
 
 function getPartnerData(index) {
 	var $partner = $($partners[index]);
-	var title = $partner.find('a').attr('title');
+	var $link = $partner.find('a');
+	var linkDestination = $link.attr('href');
+	var title = $link.attr('title');
 	var $partnerImg = $partner.find('img');
 	var img = $partnerImg.attr('src');
 	var alt = $partnerImg.attr('alt');
 	
 	var data = {
+		website: linkDestination,
 		title: title,
-		imgSrc: img,
+		imgSrc: `//www.baltimorecountyrestaurantweek.com${img}`,
 		alt: alt
 	};
 
