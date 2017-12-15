c1149c1455739bb68cd08edbf26346d103532969
Marty Powell <jpowel25@gmail.com>
2017-12-15 11:55:35 -0500

    fixed tests

--- tests/unit/mobile-nav.test.js
+++ tests/unit/mobile-nav.test.js
@@ -1,21 +1,40 @@
 /* eslint-env jasmine */
+
+
 describe('Restaurant Week', () => {
-	
+		const selectors = {
+			hamburgerIcon = '',
+			closeIcon = '',
+			mobileNavBtn = '.mobile-nav-btn',
+			navigationList = '.navigation-list'
+		};
+
+
 		beforeEach(() => {
 			
 		});
 	
 		describe('Mobile Navigation Button', () => {
-			it('should have the hamburger menu class(fa-bars) on page load', () => {
-				expect(false).to.be.eql(true);
+			it(`should have the hamburger menu class(${selectors.hamburgerIcon}) on page load`, (done) => {
+				var hasHamburgerMenuClass = $(selectors.mobileNavBtn).hasClass(selectors.hamburgerIcon);
+				expect(hasHamburgerMenuClass).to.be.eql(true);
+				done();
 			});
 
-			it('should have the hamburger menu class(fa-bars) if the mobile nav is collapsed', () => {
-				expect(false).to.be.eql(true);
+			it(`should have the hamburger menu class(${selectors.closeIcon}) if the mobile nav is collapsed`, (done) => {
+				var hasHamburgerMenuClass = $(selectors.mobileNavBtn).hasClass(selectors.closeIcon);
+				expect(hasHamburgerMenuClass).to.be.eql(true);
+				done();
 			});
 
-			it('should have the close icon class(fa-times) if the mobile nav is expanded', () => {
-				expect(false).to.be.eql(true);
+			it(`should have the close icon class${selectors.closeIcon}) if the mobile nav is expanded`, (done) => {
+				setTimeout(() => {
+					$(selectors.hamburgerIcon).click();
+					var hasHamburgerMenuClass = $(selectors.mobileNavBtn).hasClass(selectors.closeIcon);
+					expect(hasHamburgerMenuClass).to.be.eql(true);
+					done();
+				}, 500);
+				
 			});
 		});
 
