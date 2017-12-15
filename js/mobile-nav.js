f0cda301ec636167b1ebef54d6d8a6b0997a4d25
Marty Powell <jpowel25@gmail.com>
2017-12-15 12:19:47 -0500

    added logic from codepen

---
+++ js/mobile-nav.js
@@ -0,0 +1,34 @@
+namespacer("restaurantWeek");
+
+restaurantWeek.mobileNav = ($ => {
+  let self = this;
+
+  const init = options => {
+    self.options = self.options || {};
+    self.options.mobileNavButtonSelector =
+		self.options.mobileNavButtonSelector || ".mobile-nav-btn";
+    self.options.navigationListSelector =
+		self.options.navigationListSelector || ".navigation-list";
+
+    $(document).on("click", self.options.mobileNavButtonSelector, onMobileNavClick);
+  };
+
+  const onMobileNavClick = e => {
+    var $btn = $(e.currentTarget);
+    var $navList = $(self.options.navigationListSelector);
+
+    $btn
+      .find("i")
+      .toggleClass("fa-bars")
+      .toggleClass("fa-times");
+
+    $navList.toggleClass("active mobile");
+  };
+
+  return {
+    /* test-code */
+    onMobileNavClick,
+    /* end-test-code */
+    init
+  };
+})(jQuery);
