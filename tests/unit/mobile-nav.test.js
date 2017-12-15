ba22a5fbbce5a67d8f494d0ace1967e91a7137dd
Marty Powell <jpowel25@gmail.com>
2017-12-15 11:40:02 -0500

    added tests and script

---
+++ tests/unit/mobile-nav.test.js
@@ -0,0 +1,36 @@
+/* eslint-env jasmine */
+describe('Restaurant Week', () => {
+	
+		beforeEach(() => {
+			
+		});
+	
+		describe('Mobile Navigation Button', () => {
+			it('should have the hamburger menu class(fa-bars) on page load', () => {
+				expect(false).to.be.eql(true);
+			});
+
+			it('should have the hamburger menu class(fa-bars) if the mobile nav is collapsed', () => {
+				expect(false).to.be.eql(true);
+			});
+
+			it('should have the close icon class(fa-times) if the mobile nav is expanded', () => {
+				expect(false).to.be.eql(true);
+			});
+		});
+
+		describe('Mobile Navigation List', () => {
+			it('should NOT have active or mobile classes on page load', () => {
+				expect(false).to.be.eql(true);
+			});
+
+			it('should NOT have active or mobile classes on page load if the mobile nav is collapsed', () => {
+				expect(false).to.be.eql(true);
+			});
+
+			it('should have active or mobile classes if the mobile nav is expanded', () => {
+				expect(false).to.be.eql(true);
+			});
+		});
+	});
+	
