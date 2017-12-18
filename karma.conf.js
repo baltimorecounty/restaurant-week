f2f94da23136439b0503f1d0f16fedc7179783c8
Marty Powell <jpowel25@gmail.com>
2017-12-18 09:25:12 -0500

    added coverage based on bcpl project karma config

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = config => {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {},
+    preprocessors: {'js/*.js': ['coverage'] },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
