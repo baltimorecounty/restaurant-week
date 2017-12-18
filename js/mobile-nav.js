4dea2b3aaaee6f43575740f4a1ba43c0bfb27009
Marty Powell <jpowel25@gmail.com>
2017-12-18 13:57:11 -0500

    minor linting fixes

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,43 +1,37 @@
-namespacer("restaurantWeek");
+namespacer('restaurantWeek');
 
-restaurantWeek.mobileNav = ($ => {
-  let self = {};
+restaurantWeek.mobileNav = (($) => {
+	const self = {};
 
-  const init = options => {
-    self.options = self.options || {};
-    self.options.mobileNavButtonSelector =
-      self.options.mobileNavButtonSelector || ".mobile-nav-btn";
-    self.options.navigationListSelector =
-      self.options.navigationListSelector || ".navigation-list";
+	const init = (options) => {
+		self.options = options || {};
+		self.options.mobileNavButtonSelector = self.options.mobileNavButtonSelector || '.mobile-nav-btn';
+		self.options.navigationListSelector = self.options.navigationListSelector || '.navigation-list';
 
-    $(document).on(
-      "click",
-      self.options.mobileNavButtonSelector,
-      onMobileNavClick
-    );
-  };
+		$(document).on('click', self.options.mobileNavButtonSelector, onMobileNavClick);
+	};
 
-  const onMobileNavClick = e => {
-    var $btn = $(e.currentTarget);
-    var $navList = $(self.options.navigationListSelector);
+	const onMobileNavClick = (e) => {
+		const $btn = $(e.currentTarget);
+		const $navList = $(self.options.navigationListSelector);
 
-    $btn
-      .find("i")
-      .toggleClass("fa-bars")
-      .toggleClass("fa-times");
+		$btn
+			.find('i')
+			.toggleClass('fa-bars')
+			.toggleClass('fa-times');
 
-    $navList.toggleClass("active mobile");
-  };
+		$navList.toggleClass('active mobile');
+	};
 
-  return {
-    /* test-code */
-    onMobileNavClick,
-    /* end-test-code */
-    init
-  };
+	return {
+		/* test-code */
+		onMobileNavClick,
+		/* end-test-code */
+		init,
+	};
 })(jQuery);
 
-//On Document Ready
-$(document).ready(() => {
-	restaurantWeek.mobileNav.init();
-  });
+// On Document Ready
+// $(document).ready(() => {
+// 	restaurantWeek.mobileNav.init();
+// });
