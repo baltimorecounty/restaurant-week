966caf78ca7bffe8da7d35691a6506ef7eab854e
Marty Powell <jpowel25@gmail.com>
2017-12-19 10:37:56 -0500

    fixed values for templates

--- js/app/restaurant/restaurant.constants.js
+++ js/app/restaurant/restaurant.constants.js
@@ -1,9 +1,14 @@
 ((app) => {
 	const constants = {
 		urls: {
-			restaurantMockData: '',
+			templates: {
+				restaurant: 'dist/templates/restaurant/restaurant.template.html',
+				restaurantList: 'dist/templates/restaurant/restaurant-list.template.html',
+			},
+			restaurantMockData: '../../../dist/data/restaurants.json',
 		},
+		dataProvider: 'restaurantMockProvider',
 	};
 
 	app.constant('CONSTANTS', constants);
-})(angular.module('eventsPageApp'));
+})(angular.module('restaurantWeekApp'));
