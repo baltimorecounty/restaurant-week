7e0d83653a97a5d9c939db0468659a163a4812bf
Marty Powell <jpowel25@gmail.com>
2017-12-20 13:43:12 -0500

    bug fixes

--- js/app/restaurant/page-restaurant.provider.js
+++ js/app/restaurant/page-restaurant.provider.js
@@ -6,23 +6,24 @@
 		const getRestaurants = () => {
 			const list = [];
 
-			angular.element('.restaurant-list .restaurant-item')
-				.forEach((restaurantElm) => {
-					const image = angular.element(restaurantElm).find('img');
-					const link = angular.element(restaurantElm).find('.headline-link');
-					const restaurant = new RestaurantModel({
-						name: angular.element(restaurantElm).find('.restaurant-name').text(),
-						imageUrl: angular.element(image).attr('href'),
-						imageAlt: angular.element(image).attr('alt'),
-						websiteUrl: angular.element(link).attr('href'),
-						websiteUrlTitle: angular.element(link).attr('title'),
-						addressLine1: angular.element(restaurantElm).find('.restaurant-address1').text(),
-						addressLine2: angular.element(restaurantElm).find('.restaurant-address2').text(),
-						phone: angular.element(restaurantElm).find('.restaurant-phone').text(),
-					});
+			const items = angular.element('.restaurant-list .restaurant-item');
 
-					list.push(restaurant);
+			angular.forEach(items, (restaurantElm) => {
+				const image = angular.element(restaurantElm).find('img');
+				const link = angular.element(restaurantElm).find('.headline-link');
+				const restaurant = RestaurantModel({
+					name: angular.element(restaurantElm).find('.restaurant-name').text(),
+					imageUrl: angular.element(image).attr('href'),
+					imageAlt: angular.element(image).attr('alt'),
+					websiteUrl: angular.element(link).attr('href'),
+					websiteUrlTitle: angular.element(link).attr('title'),
+					addressLine1: angular.element(restaurantElm).find('.restaurant-address1').text(),
+					addressLine2: angular.element(restaurantElm).find('.restaurant-address2').text(),
+					phone: angular.element(restaurantElm).find('.restaurant-phone').text(),
 				});
+
+				list.push(restaurant);
+			});
 
 			return list;
 		};
@@ -33,5 +34,5 @@
 	};
 
 	app
-		.factory('restaurantPageProvider', ['RestaurantModel', restaurantPageProvider]);
-})(angular.module('restaurantWeekApp'));
+		.factory('restaurantPageProvider', ['rwApp.RestaurantModel', restaurantPageProvider]);
+})(angular.module('rwApp'));
