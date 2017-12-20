273e794d0d870e654540e042d991240f14d49845
Marty Powell <jpowel25@gmail.com>
2017-12-20 13:43:23 -0500

    fixed naming

--- js/app/restaurant/restaurant.constants.js
+++ js/app/restaurant/restaurant.constants.js
@@ -3,12 +3,12 @@
 		urls: {
 			templates: {
 				restaurant: 'dist/templates/restaurant/restaurant.template.html',
-				restaurantList: 'dist/templates/restaurant/restaurant-list.template.html',
+				restaurantList: 'dist/templates/restaurant-list/restaurant-list.template.html',
 			},
 			restaurantMockData: '../../../dist/data/restaurants.json',
 		},
 		dataProvider: 'restaurantMockProvider',
 	};
 
-	app.constant('CONSTANTS', constants);
-})(angular.module('restaurantWeekApp'));
+	app.constant('rwApp.CONSTANTS', constants);
+})(angular.module('rwApp'));
