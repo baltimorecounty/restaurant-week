fecbacfcecc095af8bd4c5f25cc690c9a707c38d
Marty Powell <jpowel25@gmail.com>
2017-12-21 12:40:23 -0500

    added categories and locations

--- js/app/restaurant-list/restaurant-list.controller.js
+++ js/app/restaurant-list/restaurant-list.controller.js
@@ -1,9 +1,29 @@
 ((app) => {
-	const RestaurantListCtrl = function RestaurantListCtrl($scope, restaurantService) {
-		const self = this;
+	const RestaurantListCtrl = function RestaurantListCtrl($scope, dataService, restaurantService) {
+		const vm = this;
+		vm.restaurantList = [];
+		vm.categories = [];
+		vm.locations = [];
 
-		self.restaurantList = restaurantService.getRestaurants();
+		// set the list of restaurants
+		restaurantService.getRestaurants()
+			.then((list) => {
+				vm.restaurantList = list;
+			});
+
+		// add categories for use with filter
+		dataService.getCategories()
+			.then((categories) => {
+				vm.categories = categories;
+			});
+
+
+		// add locations for use with filter
+		dataService.getLocations()
+			.then((locations) => {
+				vm.locations = locations;
+			});
 	};
 
-	app.controller('rwApp.RestaurantListCtrl', ['$scope', 'rwApp.restaurantService', RestaurantListCtrl]);
+	app.controller('rwApp.RestaurantListCtrl', ['$scope', 'rwApp.dataService', 'rwApp.restaurantService', RestaurantListCtrl]);
 })(angular.module('rwApp'));
