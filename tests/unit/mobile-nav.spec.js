2636e8886017fb35af9490e5596041dd207137fb
Marty Powell <jpowel25@gmail.com>
2017-12-22 09:03:01 -0500

    fixed mobile nav test that weren't running properly

--- tests/unit/mobile-nav.spec.js
+++ tests/unit/mobile-nav.spec.js
@@ -1,7 +1,7 @@
 /* eslint-env jasmine */
 basePath = window.__karma__ ? 'base' : ''; // eslint-disable-line
 
-describe('Restaurant Week', () => {
+describe('Mobile Navigation', () => {
 	const activeAndMobileClass = 'active mobile';
 	const selectors = {
 		hamburgerIcon: 'fa-bars',
@@ -17,16 +17,18 @@ describe('Restaurant Week', () => {
 	});
 
 	describe('Mobile Navigation Button', () => {
-		it(`should have the hamburger menu class (${selectors.hamburgerIcon}) on page load`, () => {
+		it(`should have the hamburger menu class (${selectors.hamburgerIcon}) on page load`, (done) => {
 			const hasHamburgerMenuClass = $(selectors.mobileNavBtn)
 				.find(selectors.mobileNavIcon).hasClass(selectors.hamburgerIcon);
 			expect(hasHamburgerMenuClass).toEqual(true);
+			done();
 		});
 
-		it(`should have the hamburger menu class (${selectors.hamburgerIcon}) if the mobile nav is collapsed`, () => {
+		it(`should have the hamburger menu class (${selectors.hamburgerIcon}) if the mobile nav is collapsed`, (done) => {
 			const hasHamburgerMenuClass = $(selectors.mobileNavBtn)
 				.find(selectors.mobileNavIcon).hasClass(selectors.hamburgerIcon);
 			expect(hasHamburgerMenuClass).toEqual(true);
+			done();
 		});
 
 		it(`should have the close icon class (${selectors.closeIcon}) if the mobile nav is expanded`, (done) => {
@@ -34,7 +36,6 @@ describe('Restaurant Week', () => {
 			setTimeout(() => {
 				const hasCloseMenuClass =
 					$(selectors.mobileNavBtn).find(selectors.mobileNavIcon).hasClass(selectors.closeIcon);
-
 				expect(hasCloseMenuClass).toEqual(true);
 				done();
 			}, 1000);
@@ -42,25 +43,25 @@ describe('Restaurant Week', () => {
 	});
 
 	describe('Mobile Navigation List', () => {
-		it('should NOT have active or mobile classes on page load', () => {
-			restaurantWeek.mobileNav.init();
+		it('should NOT have active or mobile classes on page load', (done) => {
 			const hasActiveAndMobileClass = $(selectors.navigationList).hasClass(activeAndMobileClass);
 			expect(hasActiveAndMobileClass).toEqual(false);
+			done();
 		});
 
-		it('should NOT have active or mobile classes on page load if the mobile nav is collapsed', () => {
-			restaurantWeek.mobileNav.init();
+		it('should NOT have active or mobile classes on page load if the mobile nav is collapsed', (done) => {
 			const hasActiveAndMobileClass = $(selectors.navigationList).hasClass(activeAndMobileClass);
 			expect(hasActiveAndMobileClass).toEqual(false);
+			done();
 		});
 
-		it('should have active or mobile classes if the mobile nav is expanded', () => {
-			restaurantWeek.mobileNav.init();
-			$(selectors.hamburgerIcon).click();
+		it('should have active or mobile classes if the mobile nav is expanded', (done) => {
+			$(selectors.mobileNavBtn).click();
 			setTimeout(() => {
 				const hasActiveAndMobileClass = $(selectors.navigationList).hasClass(activeAndMobileClass);
 				expect(hasActiveAndMobileClass).toEqual(true);
-			}, 500);
+				done();
+			}, 1000);
 		});
 	});
 });
