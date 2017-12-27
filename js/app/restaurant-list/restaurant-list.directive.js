8f258c3983a039c0de61a615601536e58ed90e9e
Marty Powell <jpowel25@gmail.com>
2017-12-27 11:55:30 -0500

    added filtering for categories

--- js/app/restaurant-list/restaurant-list.directive.js
+++ js/app/restaurant-list/restaurant-list.directive.js
@@ -10,6 +10,37 @@
 			},
 			templateUrl: constants.urls.templates.restaurantList,
 			link: (scope, element, attrs) => {
+				scope.filters = {
+					categories: ['American'],
+					locations: [],
+				};
+
+				scope.filterRestaurants = (filters) => {
+					const categories = filters.categories || [];
+					const locations = filters.locations || [];
+
+					categories.forEach((category) => {
+						if (scope.filters.categories.indexOf(category) === -1) {
+							scope.filters.categories.push(category);
+						}
+					});
+
+					locations.forEach((location) => {
+						if (scope.filters.locations.indexOf(location) === -1) {
+							scope.filters.locations.push(location);
+						}
+					});
+				};
+
+				scope.clearFilter = (name, type) => {
+					scope.filters[type] = scope.filters[type].filter(filter => filter !== name);
+				};
+
+				scope.clearFilters = () => {
+					scope.filters.categories = [];
+					scope.filters.locations = [];
+				};
+
 				const locationSearch = $location.search();
 				if (locationSearch && locationSearch.q) {
 					scope.restaurantFilter = locationSearch.q; // eslint-disable-line no-param-reassign
