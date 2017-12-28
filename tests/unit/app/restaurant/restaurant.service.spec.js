369e81f4fce831c29ec4503d9a620c59665cbf2e
Marty Powell <jpowel25@gmail.com>
2017-12-28 11:22:03 -0500

    added mock response to fix test

--- tests/unit/app/restaurant/restaurant.service.spec.js
+++ tests/unit/app/restaurant/restaurant.service.spec.js
@@ -1,4 +1,31 @@
 /* eslint no-undef: 0 */
+const mockRestaurantServiceSuccessResponse = {
+	restaurants: [
+		{
+			addressLine1: '118 Shawan Road, Suite D',
+			addressLine2: 'Hunt Valley, Maryland 21030',
+			imageUrl: '//www.baltimorecountyrestaurantweek.com/sebin/x/c/logo-barretts.jpg',
+			imageAlt: "Barrett's Grill logo",
+			websiteUrl: 'http://www.barrettsgrill.com/home',
+			websiteUrlTitle: "Link to Barrett's Grill website",
+			name: "Barrett's Grill",
+			phone: ' (410) 527-0999',
+			categories: ['American'],
+		},
+		{
+			addressLine1: '11 West Aylesbury Road',
+			addressLine2: 'Timonium, Maryland 21093',
+			imageUrl: '//www.baltimorecountyrestaurantweek.com/sebin/m/n/logo-bluestone.gif',
+			imageAlt: 'Bluestone logo',
+			websiteUrl: 'http://www.bluestoneonline.net/',
+			websiteUrlTitle: 'Link to Bluestone website',
+			name: 'BlueStone',
+			phone: ' (410) 561-1100',
+			categories: ['Fine Dining'],
+		},
+	],
+};
+
 describe('Restaurant Service', () => {
 	let contants;
 	let endpoint;
@@ -16,7 +43,7 @@ describe('Restaurant Service', () => {
 
 	describe('GetRestaurants', () => {
 		it('should hits the proper endpoint for getting restaurants as defined in the constants file', () => {
-			$httpBackend.when('GET', endpoint).respond(200, { restaurants: [{}] });
+			$httpBackend.when('GET', endpoint).respond(200, mockRestaurantServiceSuccessResponse);
 
 			restaurantService.getRestaurants()
 				.then((data) => {
