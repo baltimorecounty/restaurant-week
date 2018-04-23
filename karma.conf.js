5dc31dc92c7afb6660a7aefeee1b2b29c868facb
Marty Powell <jpowel25@gmail.com>
2018-04-23 13:16:39 -0400

    switched to firefox

--- karma.conf.js
+++ karma.conf.js
@@ -84,7 +84,7 @@ module.exports = (config) => {
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ['Chrome'],
+		browsers: ['Firefox'],
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
@@ -97,7 +97,7 @@ module.exports = (config) => {
 		plugins: [
 			'karma-jasmine-jquery',
 			'karma-mocha-reporter',
-			'karma-chrome-launcher',
+			'karma-firefox-launcher',
 			'karma-jasmine',
 			'karma-coverage',
 			'karma-coveralls',
