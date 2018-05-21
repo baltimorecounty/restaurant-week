85e283711479a4cbbda7bca2cf56e55df59e45ba
Marty Powell <jpowel25@gmail.com>
2018-05-21 14:26:45 -0400

    reformat

--- js/internal-pages.js
+++ js/internal-pages.js
@@ -5,18 +5,19 @@ namespacer('restaurantWeek');
 restaurantWeek.internalPages = (($, debounce) => {
 	const fbWidgetSelector = '.fb-page';
 
-	const updateFacebookPluginStyles = (width) => {
-		if (!isNaN(width) && window.FB) {
-			$(fbWidgetSelector).attr('data-width', width);
-			FB.XFBML.parse();
-		}
-	};
 	const getColumnWidth = () => parseFloat($(fbWidgetSelector).closest('[class^="col"]').css('width'));
 
 	const onWindowResize = () => {
 		const targetWidth = getColumnWidth();
 
 		updateFacebookPluginStyles(targetWidth);
+	};
+
+	const updateFacebookPluginStyles = (width) => {
+		if (!isNaN(width) && window.FB) {
+			$(fbWidgetSelector).attr('data-width', width);
+			FB.XFBML.parse();
+		}
 	};
 
 	const init = () => {
