2fcfbf4cd0e3c92be3e2576bce3859c386e6ad80
Marty Powell <jpowel25@gmail.com>
2018-07-05 15:09:28 -0400

    rebuild

--- public/dist/js/restaurant-week.app.js
+++ public/dist/js/restaurant-week.app.js
@@ -10,23 +10,9 @@
 })();
 'use strict';
 
-(function (app) {
-	var constants = {
-		urls: {
-			templates: {
-				restaurant: '/dist/templates/restaurant/restaurant.template.html',
-				restaurantList: '/dist/templates/restaurant-list/restaurant-list.template.html'
-			},
-			apiRoot: 'dist/data',
-			structuredContent: {
-				restaurants: '/dist/data/mock-structured-content-restaurants.json',
-				imagePath: 'http://staging.baltimorecountymd.gov'
-			}
-		}
-	};
-
-	app.constant('rwApp.CONSTANTS', constants);
-})(angular.module('rwApp'));
+(function (app, window) {
+	app.constant('rwApp.CONSTANTS', window.restaurantWeekConstants);
+})(angular.module('rwApp'), window);
 'use strict';
 
 (function (app) {
