e61f9d753fd01e5276112275f2836891f64f2729
Marty Powell <jpowel25@gmail.com>
2018-07-11 10:48:04 -0400

    removed internal pages script

--- public/dist/js/restaurant-week.js
+++ public/dist/js/restaurant-week.js
@@ -154,42 +154,6 @@ restaurantWeek.windowResize = function (debounce) {
 
 namespacer('restaurantWeek');
 
-restaurantWeek.internalPages = function ($, debounce) {
-	var fbWidgetSelector = '.fb-page';
-
-	var getColumnWidth = function getColumnWidth() {
-		return parseFloat($(fbWidgetSelector).closest('[class^="col"]').css('width'));
-	};
-
-	var onWindowResize = function onWindowResize() {
-		var targetWidth = getColumnWidth();
-
-		updateFacebookPluginStyles(targetWidth);
-	};
-
-	var updateFacebookPluginStyles = function updateFacebookPluginStyles(width) {
-		if (!isNaN(width) && window.FB) {
-			$(fbWidgetSelector).attr('data-width', width);
-			FB.XFBML.parse();
-		}
-	};
-
-	var init = function init() {
-		window.addEventListener('resize', function () {
-			debounce(function () {
-				onWindowResize();
-			}, 250);
-		});
-	};
-
-	return {
-		init: init
-	};
-}(jQuery, restaurantWeek.debounce);
-'use strict';
-
-namespacer('restaurantWeek');
-
 restaurantWeek.mobileNav = function ($, onWindowResize) {
 	var self = {};
 	var activeClass = 'active';
